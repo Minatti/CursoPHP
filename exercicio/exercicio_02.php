dc4a8cc32271198a6d1bb227865dabc369381590
luancarminatt@gmail.com <luancarminatt@gmail.com>
2018-08-10 19:29:53 -0300

    Atulizando o conteudo das aulas

---
+++ exercicio/exercicio_02.php
@@ -0,0 +1,14 @@
+<?php 
+
+$status = true;
+
+if ($status == true) {
+	
+	$liberado = 1;
+	echo $status;
+
+} else {
+
+	$liberado = 0;
+}
+?>
--- for/exemplo_03.php
+++ for/exemplo_03.php
@@ -5,8 +5,6 @@ echo "<select>";
 
 for ($i=date("Y"); $i >= date("Y")-100 ; $i--) {
 
-	var_dump("Testando");
-
 	echo '<option value="'.$i. '">'.$i.'</option>';
 }
 
