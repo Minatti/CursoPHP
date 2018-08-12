e19f46076d4a50550dc1d0e2f1dfb9a319fca387
luancarminatt@gmail.com <luancarminatt@gmail.com>
2018-08-12 20:26:54 -0300

    Metodos magicos

--- POO/exemplo_04.php
+++ POO/exemplo_04.php
@@ -1,1 +1,35 @@
-php
+<?php  
+//Métodos mágicos
+ class Endereco {
+
+ 	private $logradouro;
+ 	private $numero;
+ 	private $cidade;
+
+ 	//CONSTRUTOR
+ 	public function __construct($a, $b, $c){
+
+ 		$this->logradouro = $a;
+ 		$this->numero = $b;
+ 		$this->cidade = $c;
+ 	}
+ 	//Mandando remover da memória
+ 	public function __destruct(){
+
+ 		var_dump("DESTRUIR");
+
+
+ 	}
+
+ 	public function __toString(){
+
+ 		return $this->logradouro.", ".$this->numero." - ".$this->cidade;
+ 	}
+ }
+
+$meuEndereco = new Endereco("Rua Ademar Saraiva Leão", "123", "Santos");
+//var_dump($meuEndereco);
+//unset($meuEndereco);
+echo ($meuEndereco);
+echo "<br>";
+?>
