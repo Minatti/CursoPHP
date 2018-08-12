b178719c8f30e263fd53d21d42ac65dcd9880000
luancarminatt@gmail.com <luancarminatt@gmail.com>
2018-08-12 18:10:56 -0300

    Instanciando classes existentes do PHP

---
+++ date/exemplo_04.php
@@ -0,0 +1,6 @@
+<?php 
+//Instanciando uma classe
+$dt = new DateTime();
+
+echo $dt->format("d/m/Y H:i:s");
+ ?>
---
+++ date/exemplo_05.php
@@ -0,0 +1,16 @@
+<?php 
+//Instanciando uma classe
+
+$dt = new DateTime();
+
+$periodo = new DateInterval("P15D");
+
+echo $dt->format("d/m/Y H:i:s");
+
+$dt->add($periodo);
+
+echo "<br>";
+
+echo $dt->format("d/m/Y H:i:s");
+
+?>
