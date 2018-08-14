40e7293a5ba7d35a7bf1d9056ba44125bdf7c9a2
luancarminatt@gmail.com <luancarminatt@gmail.com>
2018-08-14 04:07:37 -0300

    alteração de dir

---
+++ PDO/exemplo_01.php
@@ -0,0 +1,19 @@
+<?php  
+//CLASSE BD
+$conexao = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
+
+$stmt = $conexao->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($results as $row) {
+	foreach ($row as $key => $value) {
+		echo "<strong>".$key.":</strong>".$value."<br/>";
+	}
+
+	echo "====================================================<br/>";
+}
+
+?>
